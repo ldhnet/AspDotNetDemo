e2241cd085f50d9ee36730a0823ef50dbc32ecea
ldhnet <574427343@qq.com>
2021-09-09 14:26:08 +0800

    Extensions

--- Console/WebMVC/HangFire/HangFireJob.cs
+++ Console/WebMVC/HangFire/HangFireJob.cs
@@ -11,10 +11,13 @@ namespace WebMVC.HangFire
     {
         public static void AddOrUpdate()
         {
-            RecurringJob.AddOrUpdate("HangFireTestId", () => Console.WriteLine("hangfire Recurring!"), Cron.Minutely(), TimeZoneInfo.Local);
+            RecurringJob.AddOrUpdate("HangFireTestId", () => Console.WriteLine("每分钟执行hangfire Recurring!"), Cron.Minutely(), TimeZoneInfo.Local);
 
             RecurringJob.AddOrUpdate("HangFireTestId2", () => Console.WriteLine("10点钟执行 hangfire Recurring!"), Cron.Daily(10), TimeZoneInfo.Local);
-             
+
+            RecurringJob.AddOrUpdate("HangFireTestId3", () => Console.WriteLine("0点钟执行 hangfire Recurring!"), Cron.Daily(), TimeZoneInfo.Local);
+
+            RecurringJob.AddOrUpdate("HangFireTestId4", () => Console.WriteLine("1点钟执行 hangfire Recurring!"), Cron.Daily(1), TimeZoneInfo.Local);
         }
     }
 }
