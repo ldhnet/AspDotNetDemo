73407ae9c68225865da4fa67b079a4108a535cff
ldhnet <574427343@qq.com>
2021-10-26 11:20:50 +0800

    DataEncryption 数据库字段加密

--- Console/ConsoleDBTest/DefaultDbContext.cs
+++ Console/ConsoleDBTest/DefaultDbContext.cs
@@ -16,12 +16,15 @@ namespace ConsoleDBTest
         //private static string ConnectionString = ConfigurationManager.AppSettings.Get("ConnectionStringName") ?? "default";
         private static string connstring = "Data Source=.;Initial Catalog=DHDatabase;user id=sa;password=2021@ldh";
  
-        private readonly byte[] _encryptionKey = AesProvider.GenerateKey(AesKeySize.AES256Bits).Key; 
+        private readonly byte[] _encryptionKey = Encoding.Default.GetBytes("4A7D1ED414474E4033AC29CCB8653D9B");
+        private readonly byte[] _encryptionIV = Encoding.Default.GetBytes("0019DA6F1F30D07C51EBC5FCA1AC7DA6".Substring(0, 16));
+
         private readonly IEncryptionProvider _provider;
          
         public DefaultDbContext()
         { 
-            this._provider = new AesProvider(this._encryptionKey);
+            //this._provider = new AesProvider(this._encryptionKey);
+            this._provider = new AesProvider(this._encryptionKey, this._encryptionIV);
         }
         public DbSet<Employee> Employees { get; set; } 
 
@@ -36,5 +39,5 @@ namespace ConsoleDBTest
              
             base.OnModelCreating(modelBuilder);
         }
-    }
+    }  
 }
--- Console/ConsoleDBTest/Program.cs
+++ Console/ConsoleDBTest/Program.cs
@@ -22,14 +22,14 @@ namespace ConsoleDBTest
             var users = context.Employees.ToList();
 
 
-            Employee zhangsan = new Employee { Name = "张三", BankCard = "12345", BankCardDisplay = "12345", Monery = "11.1", MoneryDisplay = "11.1" };
-            Employee lisi = new Employee { Name = "李四", BankCard = "67890", BankCardDisplay = "67890", Monery = "12.2", MoneryDisplay = "11.1" };
+            Employee zhangsan = new Employee { Name = Guid.NewGuid().ToString(), BankCard = "123456789101112131415", BankCardDisplay = "123456789101112131415", Monery = "11.1", MoneryDisplay = "11.1" };
+            Employee lisi = new Employee { Name = Guid.NewGuid().ToString(), BankCard = "987654312222222233344", BankCardDisplay = "987654312222222233344", Monery = "12.2", MoneryDisplay = "11.1" };
             context.Employees.AddRange(zhangsan, lisi);
             context.SaveChanges();
 
             foreach (var user in users)
             {
-                Console.WriteLine($"{user.Id} {user.Name} {user.BankCard}");
+                Console.WriteLine($"{user.Id} {user.Name} {user.BankCardDisplay} {user.BankCard}");
             }
              
 
