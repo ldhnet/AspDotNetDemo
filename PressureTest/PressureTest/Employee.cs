b6d750d636628fe857c2b46f61b52ec809f540d5
ldhnet <574427343@qq.com>
2021-10-29 09:12:12 +0800

    Pressure UnitTest

---
+++ PressureTest/PressureTest/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks; 
+
+namespace PressureTest
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string EmployeeName { get; set; } 
+        public string Age { get; set; }
+        public string EnglishName { get; set; }
+
+        public string EmployeeSerialNumber { get; set; }
+
+        public int? Department { get; set; } 
+        public string Phone { get; set; }
+         
+        public string BankCard { get; set; } 
+        public string BankCardDisplay { get; set; }
+         
+        public string Monery { get; set; } 
+        public string MoneryDisplay { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} -- {BankCardDisplay}";
+        }
+    }
+}
---
+++ PressureTest/PressureTest/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using PressureTest;
+
+
+string url = "http://localhost:6001/Home/PostImfo";
+
+HttpResult httpResult = new HttpHelper().GetHtml(new HttpItem
+{
+    URL = "http://localhost:6001/Home/Privacy",
+    ContentType = "text/html; charset=gb2312"
+});
+ 
+try
+{ 
+    var dto = new Employee
+    {
+        Name = "tom"
+    };
+    for (int i = 0; i < 1200; i++)
+    {
+        var strResult = HttpHelper.HttpPost(url, JsonHelper.ToJson(dto));
+        var result = JsonHelper.FromJson<Employee>(strResult);
+   
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.WriteLine("1--Hello, World!");
+
+Console.WriteLine("2--Hello, World!");
+
+Console.WriteLine("Hello, World!");
