26c7f72dcb0d9c51175d78875bf723ba0badad2b
ldhnet <574427343@qq.com>
2021-11-02 14:33:48 +0800

    UnitTest

--- Console/UnitTest/UnitTest1.cs
+++ Console/UnitTest/UnitTest1.cs
@@ -3,6 +3,7 @@ using WebMVC.Security;
 using Xunit;
 
 using Shouldly;
+using WebMVC.Extension;
 
 namespace UnitTest
 {
@@ -14,6 +15,42 @@ namespace UnitTest
             string key = new AesHelper().Key;
             Convert.FromBase64String(key).Length.ShouldBe(32);
         }
+        [Fact]
+        public void Encrypt_Decrypt_Test()
+        {
+            AesHelper aes = new AesHelper();
+            string source = "admin";
 
+            //byte[]
+            byte[] sourceBytes = source.ToBytes();
+            byte[] enBytes = aes.Encrypt(sourceBytes);
+            aes.Decrypt(enBytes).ShouldBe(sourceBytes);
+
+            //string
+            string enstr = aes.Encrypt(source);
+            aes.Decrypt(enstr).ShouldBe(source);
+
+            aes = new AesHelper(true);
+
+            //byte[]
+            enBytes = aes.Encrypt(sourceBytes);
+            aes.Decrypt(enBytes).ShouldBe(sourceBytes);
+
+            //string
+            enstr = aes.Encrypt(source);
+            aes.Decrypt(enstr).ShouldBe(source);
+        }
+
+        [Fact]
+        public void Encrypt_Decrypt_File_Test()
+        {
+            string file = @"OSharp.Tests.dll", enFile = "OSharp.Tests_en.dll", deFile = "OSharp.Tests_de.dll";
+            AesHelper aes = new AesHelper();
+            aes.EncryptFile(file, enFile);
+            aes.DecryptFile(enFile, deFile);
+
+            file.ShouldFileMd5Be(deFile);
+            enFile.ShouldFileMd5NotBe(deFile);
+        }
     }
 }
