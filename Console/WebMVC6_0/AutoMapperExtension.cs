47c2d9b1056f89eadc59397c16a02a7a46c6de47
lee.li <574427343@qq.com>
2021-12-06 16:40:00 +0800

    console

--- Console/WebMVC6_0/AutoMapperExtension.cs
+++ Console/WebMVC6_0/AutoMapperExtension.cs
@@ -1,32 +1,32 @@
-﻿using AutoMapper;
-using AutoMapper.Configuration;
-using Framework.Mapper; 
+﻿//using AutoMapper;
+//using AutoMapper.Configuration;
+//using Framework.Mapper; 
 using Microsoft.Extensions.DependencyInjection.Extensions; 
 namespace WebMVC6_0.Extensions
 {
     public static class AutoMapperExtension
     {
-        public static IServiceCollection AddAutoMapper(this IServiceCollection service)
-        {
-            service.TryAddSingleton<MapperConfigurationExpression>(); 
+        //public static IServiceCollection AddAutoMapper(this IServiceCollection service)
+        //{
+        //    service.TryAddSingleton<MapperConfigurationExpression>(); 
 
-            service.TryAddSingleton(serviceProvider =>
-            {
-                var mapperConfigurationExpression = serviceProvider.GetRequiredService<MapperConfigurationExpression>();
-                mapperConfigurationExpression.AddProfile(new MapperProfile());
-                var instance = new MapperConfiguration(mapperConfigurationExpression);
-                instance.AssertConfigurationIsValid();
-                return instance;
-            });
+        //    service.TryAddSingleton(serviceProvider =>
+        //    {
+        //        var mapperConfigurationExpression = serviceProvider.GetRequiredService<MapperConfigurationExpression>();
+        //        mapperConfigurationExpression.AddProfile(new MapperProfile());
+        //        var instance = new MapperConfiguration(mapperConfigurationExpression);
+        //        instance.AssertConfigurationIsValid();
+        //        return instance;
+        //    });
 
-            service.TryAddSingleton(serviceProvider =>
-            {
-                var mapperConfiguration = serviceProvider.GetRequiredService<MapperConfiguration>(); 
-                return mapperConfiguration.CreateMapper();
-            });
+        //    service.TryAddSingleton(serviceProvider =>
+        //    {
+        //        var mapperConfiguration = serviceProvider.GetRequiredService<MapperConfiguration>(); 
+        //        return mapperConfiguration.CreateMapper();
+        //    });
 
-            return service;
-        }
+        //    return service;
+        //}
          
       
     }
