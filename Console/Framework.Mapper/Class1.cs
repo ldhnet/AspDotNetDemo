fa9162a9468569cc023ad131e7a8e89380539c9e
lee.li <574427343@qq.com>
2021-12-06 16:31:54 +0800

    console

---
+++ Console/Framework.Mapper/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Mapper
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+    public class PersonDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ Console/Framework.Mapper/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Framework.Mapper
+{
+    public class MapperProfile : Profile, IProfile
+    { 
+        public MapperProfile() {
+            CreateMap<Person, PersonDto>().ForMember(d => d.ID, s => s.Ignore());
+            CreateMap<PersonDto, Person>();
+         }
+    }
+}
