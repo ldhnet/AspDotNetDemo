a22c9973499c89346cad138c7d8d0bcc8e94e14e
lee.li <574427343@qq.com>
2021-12-28 10:53:09 +0800

    Abstract test

---
+++ Console/ConsoleApp/Rule/AbstractAuditor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Rule
+{
+    public abstract class AbstractAuditor
+    {
+        public string Name { get; set; }
+        public AbstractAuditor _NextAuditor = null;
+        public void SetNextAuditor(AbstractAuditor abstractAuditor)
+        { 
+            this._NextAuditor = abstractAuditor;    
+        }
+        public abstract void Audit(ApplyContext applyContext);
+    }
+}
---
+++ Console/ConsoleApp/Rule/ApplyContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Rule
+{
+    public class ApplyContext
+    {
+        public int Hour { get; set; }
+        public string Auditor { get; set; }
+        public bool AuditResult{ get; set; }
+    }
+}
