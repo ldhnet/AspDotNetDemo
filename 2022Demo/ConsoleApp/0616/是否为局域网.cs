0df41151c4adb4fc7b0b72a36e15ed0cde6d9cf7
ldhnet <574427343@qq.com>
2022-06-19 09:39:21 +0800

    Is Private Network

---
+++ 2022Demo/ConsoleApp/0616/是否为局域网.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp._0616
+{
+    public class 是否为局域网
+    {
+        public bool IsPrivateNetwork(string ipv4Address)
+        {
+            if (IPAddress.TryParse(ipv4Address, out _))
+            {
+                if (ipv4Address.StartsWith("192.168.") || ipv4Address.StartsWith("10."))
+                {
+                    return true;
+                }
+                if (ipv4Address.StartsWith("172."))
+                {
+                    string seg2=ipv4Address[4..7];
+                    if (seg2.EndsWith(".") && string.Compare(seg2,"16.") >= 0 && string.Compare(seg2, "31.") <= 0)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+    }
+}
